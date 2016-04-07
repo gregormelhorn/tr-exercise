2dd8e0a0eff5a020955adb82bd461ff7b045e0c8
Gregor Melhorn <gregor.melhorn@gmail.com>
2016-04-07 11:35:45 +0200

    added network from requirements to tests

--- test/trains/network_test.rb
+++ test/trains/network_test.rb
@@ -2,18 +2,21 @@ require 'test_helper'
 
 class NetworkTest < Minitest::Test
   def setup
-    @network = Network.new('AB1, AC2')
+    @network = Network.new File.read('input.txt')
   end
 
   def test_scan_data
-    assert_equal 2, @network.routes.length
-    assert_equal({from: 'A', to: 'B', distance: 1}, @network.routes.first)
+    assert_equal 9, @network.routes.length
+    assert_equal({from: 'A', to: 'B', distance: 5}, @network.routes.first)
+    assert_equal({from: 'A', to: 'E', distance: 7}, @network.routes.last)
   end
 
   def test_neighbors
-    assert_equal [], @network.neighbors('B')
-    assert_equal [], @network.neighbors('C')
-    assert_equal [], @network.neighbors('D')
-    assert_equal ['B', 'C'], @network.neighbors('A')
+    assert_equal ['B', 'D', "E"], @network.neighbors('A')
+    assert_equal ["C"], @network.neighbors('B')
+    assert_equal ["D","E"], @network.neighbors('C')
+    assert_equal ["C", "E"], @network.neighbors('D')
+    assert_equal ["B"], @network.neighbors('E')
+    assert_equal [], @network.neighbors('F')
   end
 end
